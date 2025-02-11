c9533970ed0e4e9755315e285859d91fa9d1c6ad
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-11 15:46:03 +0100

    commit 1

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,74 +1,45 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
 
 class CommentArea extends Component {
   state = {
     comments: [],
     isLoading: false,
     isError: false,
-  }
-
-  // componentDidMount = async () => {
-  //   try {
-  //     let response = await fetch(
-  //       'https://striveschool-api.herokuapp.com/api/comments/' +
-  //         this.props.asin,
-  //       {
-  //         headers: {
-  //           Authorization:
-  //             'Bearer inserisci-qui-il-tuo-token',
-  //         },
-  //       }
-  //     )
-  //     console.log(response)
-  //     if (response.ok) {
-  //       let comments = await response.json()
-  //       this.setState({ comments: comments, isLoading: false, isError: false })
-  //     } else {
-  //       console.log('error')
-  //       this.setState({ isLoading: false, isError: true })
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //     this.setState({ isLoading: false, isError: true })
-  //   }
-  // }
+  };
 
   componentDidUpdate = async (prevProps) => {
     if (prevProps.asin !== this.props.asin) {
       this.setState({
         isLoading: true,
-      })
+      });
       try {
-        let response = await fetch(
-          'https://striveschool-api.herokuapp.com/api/comments/' +
-            this.props.asin,
-          {
-            headers: {
-              Authorization: 'Bearer inserisci-qui-il-tuo-token',
-            },
-          }
-        )
-        console.log(response)
+        let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2E0ZGUxYmNhMDcwNDAwMTU4YmY5NzkiLCJpYXQiOjE3Mzg4NTgwMTEsImV4cCI6MTc0MDA2NzYxMX0.KY1i3aAaFytdpVHLectYt_unBT7ZsLQJtlf6z-iXCXg",
+          },
+        });
+        console.log(response);
         if (response.ok) {
-          let comments = await response.json()
+          let comments = await response.json();
           this.setState({
             comments: comments,
             isLoading: false,
             isError: false,
-          })
+          });
         } else {
-          this.setState({ isLoading: false, isError: true })
+          this.setState({ isLoading: false, isError: true });
         }
       } catch (error) {
-        console.log(error)
-        this.setState({ isLoading: false, isError: true })
+        console.log(error);
+        this.setState({ isLoading: false, isError: true });
       }
     }
-  }
+  };
 
   render() {
     return (
@@ -78,8 +49,8 @@ class CommentArea extends Component {
         <AddComment asin={this.props.asin} />
         <CommentList commentsToShow={this.state.comments} />
       </div>
-    )
+    );
   }
 }
 
-export default CommentArea
+export default CommentArea;
